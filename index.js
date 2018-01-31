b6933dca8611a6803dc92b56a2b4dd50d875cfd6
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-01-31 20:50:55 +0000

    Install nodemon to handle listen

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/api', function(req, res){
+    console.log('GET request');
+    res.send({ name: 'Yoshi'});
+});
+
 // listen for requests
 
 app.listen(process.env.port || 4000, function() {
-
+    console.log('now listening for requests'); 
 });
